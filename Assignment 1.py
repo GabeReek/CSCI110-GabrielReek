2c83e901bdce5b3ccbeb7be574725acf6dcee862
GabeReek <greek@mavs.coloradomesa.edu>
2026-04-06 19:42:18 -0600

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,52 @@
+Python 3.14.3 (v3.14.3:323c59a5e34, Feb  3 2026, 11:41:37) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> G=Gabe
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    G=Gabe
+NameError: name 'Gabe' is not defined
+>>> n=Gabe
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    n=Gabe
+NameError: name 'Gabe' is not defined
+>>> Gabe=n
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    Gabe=n
+NameError: name 'n' is not defined
+>>> n=17
+>>> n
+17
+>>> b=gabe
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    b=gabe
+NameError: name 'gabe' is not defined
+>>> z=alright
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    z=alright
+NameError: name 'alright' is not defined
+>>> z=AlrightThenFolks
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    z=AlrightThenFolks
+NameError: name 'AlrightThenFolks' is not defined
+>>> z="Gabe"
+>>> z
+'Gabe'
+>>> print(n,z)
+17 Gabe
+>>> A="All"
+>>> w="Work"
+>>> a="and"
+>>> n="no"
+>>> p="play"
+>>> m="makes"
+>>> J="Jack"
+>>> B="a"
+>>> d="dull"
+>>> b="boy."
+>>> print(A,w,a,n,p,m,J,B,d,b)
+All Work and no play makes Jack a dull boy.
