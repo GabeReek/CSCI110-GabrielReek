9780709f2d37c50a2cbabe531740c94a207e8f14
GabeReek <greek@mavs.coloradomesa.edu>
2026-04-06 19:48:11 -0600

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,17 @@
+Python 3.14.3 (v3.14.3:323c59a5e34, Feb  3 2026, 11:41:37) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> import turtle
+>>> wn=turtle.Screen()
+>>> gabe=turtle.Turtle()
+>>> gabe.forward(200)
+>>> gabe.right(144)
+>>> gabe.forward(200)
+>>> gabe.right(144)
+>>> gabe.forward(200)
+>>> gabe.right(144)
+>>> gabe.forward(200)
+>>> gabe.right(144)
+>>> gabe.forward(200)
+>>> gabe.hideturtle
+<bound method TPen.hideturtle of <turtle.Turtle object at 0x109b9d7f0>>
+>>> gabe.hideturtle()
